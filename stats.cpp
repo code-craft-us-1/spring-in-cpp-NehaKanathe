edd55a3670556f745fc315a1cbb1d2ecdbba9850
NehaKanathe <111957313+NehaKanathe@users.noreply.github.com>
2024-08-23 17:14:05 +0000

    changes1

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,24 @@
 #include "stats.h"
+#include <vector>
+#include <numeric>
+#include <limits>
+#include <cmath>
+#include <array>
+#include <algorithm>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& data) {
     //Implement statistics here
+    Stats stats;
+    if(data.empty())
+    {
+        stats.average = std::nanf("");
+        stats.max=std::nanf("");
+        stats.min=std::nanf("");
+        return stats;
+    }
+    float sum=std::accumulate(data.begin(),data.end(),0.0f);
+    stats.average = sum/ data.size();
+    stats.max =*std::max_element(data.begin(),data.end());
+    stats.min = *std::min_element(data.begin,data.end());
+    return stats;
 }
--- stats.h
+++ stats.h
@@ -2,6 +2,12 @@
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+    public:
+    struct Stats
+    {
+        float average;
+        float max;
+        float min;
+    }
+    Stats ComputeStatistics(const std::vector<float>& );
 }
